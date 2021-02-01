b2acc4ab5f28195227cbb71de919822021bcbbb8
John Delizo <johndelizo99@gmail.com>
2021-01-31 22:53:11 -0800

    created mongodb connection

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+const mongoClient = require("mongodb").MongoClient;
+mongoClient.connect("mongodb://localhost:27017", { useNewUrlParser: true }, (error,client) => {
+    let blog = client.db("blog");
+    console.log("DB Connected")
+} ),
 
+app.get("/", (req, res) => {
+     res.send("Hello World!!!")
+})
 
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
