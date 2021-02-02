fa4d4ceafcd06bfafca371b7a3e821233b7ce8a8
John Delizo <johndelizo99@gmail.com>
2021-02-02 02:47:27 -0800

    added code to patch error

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ const app = express();
 const PORT = 5000;
 
 const mongoClient = require("mongodb").MongoClient;
-mongoClient.connect("mongodb://localhost:27017", { useNewUrlParser: true }, (error,client) => {
-    let blog = client.db("blog");
+mongoClient.connect("mongodb://localhost:27017", {useUnifiedTopology: true}, { useNewUrlParser: true }, (error, client) => {
+    const blog = client.db("blog");
     console.log("DB Connected")
-} ),
 
-app.get("/", (req, res) => {
-     res.send("Hello World!!!")
-})
-
-
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+    app.get("/", (req, res) => {
+        res.send("Hello World!!!")
+   });
+   
+   
+   app.listen(PORT, () => console.log(`http://localhost:${PORT}`
+   ));
+});
